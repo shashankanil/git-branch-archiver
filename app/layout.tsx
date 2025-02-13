33518ac7d5c966f5eb8fe296983479cd5809ec5b
shashankanil <shashankmay6@gmail.com>
2025-02-13 19:49:55 +0530

    branding removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'github-archiver',
+  description: 'archive and tag your github repositories',
+  generator: 'github-archiver',
 }
 
 export default function RootLayout({
